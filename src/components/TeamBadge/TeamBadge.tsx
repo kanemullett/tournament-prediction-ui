4bd7f5d9b5aa93d58d9a5fb9691d6f9a83b70c3d
Kane Mullett <kane.mullett@savient.uk.com>
2025-06-04 21:14:48 +0100

    Remove old badge stuff

--- src/components/TeamBadge/TeamBadge.tsx
+++ src/components/TeamBadge/TeamBadge.tsx
@@ -1,64 +1,16 @@
-import * as Flags from "country-flag-icons/react/3x2";
 import React from 'react';
 import { StyledTeamBadge } from "./TeamBadge.styles.tsx";
-import EnglishFlag from "../flags/EnglishFlag.tsx";
-import ScottishFlag from "../flags/ScottishFlag.tsx";
-import WelshFlag from "../flags/WelshFlag.tsx";
-import NorthernIrishFlag from "../flags/NorthernIrishFlag.tsx";
 
 interface ITeamBadge {
     imagePath?: string;
 }
 
 const TeamBadge = (props: ITeamBadge) => {
-    const FlagComponent = props.imagePath 
-    ? Flags[props.imagePath.toUpperCase() as keyof typeof Flags] 
-    : undefined;
-
-    if (FlagComponent) {
-        return (
-            <StyledTeamBadge>
-                <FlagComponent />
-            </StyledTeamBadge>
-        );
-    }
-
-    if (props.imagePath?.toUpperCase() === "GB-ENG") {
-        return (
-            <StyledTeamBadge>
-                <EnglishFlag />
-            </StyledTeamBadge>
-        )
-    }
-
-    if (props.imagePath?.toUpperCase() === "GB-WLS") {
-        return (
-            <StyledTeamBadge>
-                <WelshFlag />
-            </StyledTeamBadge>
-        )
-    }
-
-    if (props.imagePath?.toUpperCase() === "GB-SCT") {
-        return (
-            <StyledTeamBadge>
-                <ScottishFlag />
-            </StyledTeamBadge>
-        )
-    }
-
-    if (props.imagePath?.toUpperCase() === "GB-NIR") {
-        return (
-            <StyledTeamBadge>
-                <NorthernIrishFlag />
-            </StyledTeamBadge>
-        )
-    }
-
+    
     if (props.imagePath?.startsWith("http")) {
         return (
             <StyledTeamBadge>
-                <img src={props.imagePath} alt="badge"  ></img>
+                <img src={props.imagePath} alt="badge"></img>
             </StyledTeamBadge>
         )
     }
